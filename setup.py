691a5d3163c5c96cd0d855f1473a6512f9032f75
galaunay <gaby.launay@tutanota.com>
2019-05-21 21:57:29 +0100

    Updated dependencies

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     ],
     keywords='GUI DSA drop shape contact angle hysteresis',
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'samples']),
-    install_requires=['pyQt5', 'pyDSA_core==1.0.2', 'IMTreatment==1.0.0',
+    install_requires=['pyQt5', 'pyDSA_core==1.1.1', 'IMTreatment==1.1.1',
                       'numpy', 'matplotlib>=2.2.0'],
     extras_require={},
     setup_requires=['pytest-runner'],
